eaaeb61ab3bda4307f3839f420d28a885c1dff0f
Connor Wiseman <wiseman.connor@gmail.com>
2018-01-23 13:45:42 -0700

    ContactDetail component

--- cms/src/app/contacts/contact-detail/contact-detail.component.ts
+++ cms/src/app/contacts/contact-detail/contact-detail.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Contact } from '../contacts.model';
+
 @Component({
-  selector: 'cms-contacts-detail',
-  templateUrl: './contacts-detail.component.html',
-  styleUrls: ['./contacts-detail.component.css']
+  selector: 'cms-contact-detail',
+  templateUrl: './contact-detail.component.html',
+  styleUrls: ['./contact-detail.component.css']
 })
-export class ContactsDetailComponent implements OnInit {
+export class ContactDetailComponent implements OnInit {
+  public contact: Contact;
 
   constructor() { }
 

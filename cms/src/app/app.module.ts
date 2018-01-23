6807ffb089fe7e45834846dcabf3bcf8ff9890fe
Connor Wiseman <wiseman.connor@gmail.com>
2018-01-23 12:57:24 -0700

    Root component

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -3,16 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactsDetailComponent } from './contacts/contacts-detail/contacts-detail.component';
+import { HeaderComponent } from './header.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactsComponent,
+    ContactListComponent,
+    ContactsDetailComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent
+  ]
 })
 export class AppModule { }

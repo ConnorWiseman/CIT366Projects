0c22f354c36774260e2c1a951cd563357d44dfca
Connor Wiseman <wiseman.connor@gmail.com>
2018-01-28 09:54:28 -0700

    Lesson 3

--- cms/src/app/documents/document-detail/document-detail.component.ts
+++ cms/src/app/documents/document-detail/document-detail.component.ts
@@ -2,14 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'cms-document-detail',
-  templateUrl: './document-detail.component.html',
-  styleUrls: ['./document-detail.component.css']
+  templateUrl: './document-detail.component.html'
 })
 export class DocumentDetailComponent implements OnInit {
-
   constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() { }
 }

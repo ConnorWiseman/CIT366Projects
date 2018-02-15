b4be3715a1c12f8ad21fa067c2f8e54bd5d3399c
Connor Wiseman <wiseman.connor@gmail.com>
2018-02-15 11:16:45 -0700

    Fixed directive

--- cms/src/app/shared/dropdown.directive.ts
+++ cms/src/app/shared/dropdown.directive.ts
@@ -4,13 +4,15 @@ import { Directive, HostListener, HostBinding } from '@angular/core';
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
-  @HostBinding('class.open') isOpen = false;
+  @HostBinding('class.show') isOpen = false;
 
   @HostListener('click') toggleOpen() {
+    console.log('Click');
     this.isOpen = !this.isOpen;
   }
 
   @HostListener('mouseleave') close() {
+    console.log('MouseLeave');
     this.isOpen = false;
   }
 }

247ced1ced58e257150f35f9da16fe388c9cb84b
Connor Wiseman <wiseman.connor@gmail.com>
2018-02-15 10:24:33 -0700

    Added to directive

--- cms/src/app/shared/dropdown.directive.ts
+++ cms/src/app/shared/dropdown.directive.ts
@@ -9,4 +9,8 @@ export class DropdownDirective {
   @HostListener('click') toggleOpen() {
     this.isOpen = !this.isOpen;
   }
+
+  @HostListener('mouseleave') close() {
+    this.isOpen = false;
+  }
 }
